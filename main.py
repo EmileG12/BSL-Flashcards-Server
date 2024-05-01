709b6239865bad559b820667737e8c5b2e295d7a
EmileG12 <aimilios.gaston@gmail.com>
2024-05-01 15:03:02 +0100

    Updated to serve video for testing

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import string
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import os
+
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -17,6 +21,7 @@ def read_decklist():
     print("Received call")
     return decklist
 
+
 @app.get("/decklist/{deckname}")
 def read_deck(deckname: str):
     if deckname == "Ronaldo":
@@ -24,6 +29,26 @@ def read_deck(deckname: str):
     elif (deckname == "Neymar"):
         return {"Selecao": "Canarinha", }
 
+
+@app.get("/videofileresponse")
+async def video_endpoint():
+
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "answers/hello.mp4"
+    video_path = os.path.join(absolute_path, relative_path)
+    print("Requested video")
+    return FileResponse(video_path)
+
+
+@app.get("/decklist/videotest")
+async def video_endpoint():
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "answers/hello.mp4"
+    video_path= os.path.join(absolute_path, relative_path)
+    def iterfile():
+        with open(video_path, mode="rb") as file_like:
+            yield from file_like
+    return StreamingResponse(iterfile(), media_type="video/mp4")
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):

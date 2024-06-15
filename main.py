d81c139652ce7b9c333020840cae7efd424b34e3
EmileG12 <aimilios.gaston@gmail.com>
2024-06-15 18:25:31 +0100

    Methods for serving video
    Added method for acquiring decks in folder, sending list of words in deck
    Added path for requesting a specific video from a specific deck
    TODO : Clean up, error handling

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 import os
-
+import re
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+def sanitize_paths(path):
+    return re.sub(r'\W+', '', path)
 
 
 @app.get("/")
@@ -17,37 +19,61 @@ def read_root():
 
 @app.get("/decklist")
 def read_decklist():
-    decklist = ["Ronaldo", "Neymar"]
-    print("Received call")
-    return decklist
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "answers/"
+    answers_path = os.path.join(absolute_path, relative_path)
+    return os.listdir(answers_path)
 
 
 @app.get("/decklist/{deckname}")
 def read_deck(deckname: str):
-    if deckname == "Ronaldo":
-        return {"Real":"Madrid", "Paris":"Saint Germain"}
-    elif (deckname == "Neymar"):
-        return {"Selecao": "Canarinha", }
+    deckname = sanitize_paths(deckname)
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "answers/"
+    answers_path = os.path.join(absolute_path, relative_path)
+    deck_path = os.path.join(answers_path, deckname)
+    return os.listdir(deck_path)
 
 
 @app.get("/videofileresponse")
 async def video_endpoint():
 
     absolute_path = os.path.dirname(__file__)
-    relative_path = "answers/hello.mp4"
+    relative_path = "answers/Helloworld/hello.mp4"
     video_path = os.path.join(absolute_path, relative_path)
     print("Requested video")
     return FileResponse(video_path)
 
+def create_vid_path(params:list = []):
+    absolute_path = os.path.dirname(__file__)
 
-@app.get("/decklist/videotest")
+    for i in params:
+        absolute_path = os.path.join(absolute_path, i)
+
+    return absolute_path
+
+@app.get("/getvideo/")
+async def get_video(deckname: str = "None", videoname: str = "None"):
+    video_path = create_vid_path(["answers",deckname,videoname])
+    async def iterfile():
+        with open(video_path, mode="rb") as file_like:
+            for chunk in iter(lambda: file_like.read(65536), b""):
+                yield chunk
+                
+    return StreamingResponse(iterfile(), media_type="video/mp4")
+
+
+@app.get("/videotest")
 async def video_endpoint():
     absolute_path = os.path.dirname(__file__)
-    relative_path = "answers/hello.mp4"
-    video_path= os.path.join(absolute_path, relative_path)
-    def iterfile():
+    relative_path = r"answers/Helloworld/hello.mp4"
+    video_path = os.path.join(absolute_path, relative_path)
+    print(video_path)
+    async def iterfile():
         with open(video_path, mode="rb") as file_like:
-            yield from file_like
+            for chunk in iter(lambda: file_like.read(65536), b""):
+                yield chunk
+
     return StreamingResponse(iterfile(), media_type="video/mp4")
 
 @app.get("/items/{item_id}")
